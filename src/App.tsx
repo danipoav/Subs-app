b125b23e16cf4f365b3f57f9115cda6940cd4eea
danipoav <dapoav2002@gmail.com>
2025-03-19 19:47:20 +0100

    Adding hero img

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import heroImage from "./assets/prueba2.webp";
+
 
 function App() {
 
   return (
     <>
-      <div style={{ background: "#08090a" }} className=" min-h-screen text-white flex flex-col items-center">
-        <div className=" w-full max-w-5xl px-6">
+      <div className=" min-h-screen text-white flex flex-col items-center bg-black">
+        <div className=" w-full max-w-6xl px-6">
           <Navbar />
           <Hero />
         </div>
+        <img src={heroImage} alt="Heroimg" className=" w-2/4" style={{
+          WebkitMaskImage: "radial-gradient(circle, rgba(0,0,0,1) 10%, rgba(0,0,0,0) 95%)",
+          maskImage: "radial-gradient(circle, rgba(0,0,0,1) 50%, rgba(0,0,0,0) 95%)",
+        }}/>
       </div>
     </>
   )
