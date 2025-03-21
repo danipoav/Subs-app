400882e2bed3f1e5c4b569e743880f0f6488b1e6
danipoav <dapoav2002@gmail.com>
2025-03-22 00:29:34 +0100

    Closing fetcher

--- src/features/api/fetcher.ts
+++ src/features/api/fetcher.ts
@@ -1,6 +1,25 @@
+export const fetcher = async (endpoint: String, options: RequestInit = {}) => {
 
-const url = process.env.REACT_APP_API_URL;
+    const url = 'http://localhost:8080/api';
+    const token = localStorage.getItem('token');
 
-export const fetcher = (endpoint: String) => {
-    
+    try {
+        const response = await fetch(`${url}/${endpoint}`, {
+            ...options,
+            headers: {
+                'Content-Type': "application/json",
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        if (response.ok) {
+            return await response.json();
+        } else {
+            const error = await response.json();
+            throw new Error(error.message || 'Error fetching API');
+        }
+    } catch (error) {
+        console.log(`Error fetching on URL: ${endpoint}`, error)
+        throw error;
+    }
+
 }
