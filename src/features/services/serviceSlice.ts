205ce0d7cc031cb829e159bb24b6c126c86fb219
danipoav <dapoav2002@gmail.com>
2025-03-24 13:03:50 +0100

    Creating service slice for showing all services

---
+++ src/features/services/serviceSlice.ts
@@ -0,0 +1,44 @@
+import { getAllServices } from "./serviceThunk";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Service {
+    id: number,
+    name: String,
+    description: String,
+    logo: String,
+}
+
+interface ServiceState {
+    loading: boolean,
+    error: null | string,
+    services: Service[]
+}
+
+const initialState: ServiceState = {
+    loading: false,
+    error: null,
+    services: []
+}
+
+const serviceSlice = createSlice({
+    name: 'service',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAllServices.fulfilled, (state, action) => {
+                state.services = action.payload;
+                state.error = null;
+                state.loading = false;
+            })
+            .addCase(getAllServices.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(getAllServices.rejected, (state) => {
+                state.error = "Error fetching services";
+                state.loading = false;
+            })
+    }
+});
+
+export default serviceSlice.reducer;
