421947b857b926ee015f75734f2ef4dd3527eaa7
danipoav <dapoav2002@gmail.com>
2025-04-01 13:08:18 +0200

    Creating button disabled when some user is subscribed to some plan of this service

--- src/components/ServicesSlider.tsx
+++ src/components/ServicesSlider.tsx
@@ -9,7 +9,9 @@ export default function ServicesSlider() {
 
     const services = useSelector((state: RootState) => state.service.services);
     const plans = useSelector((state: RootState) => state.plan.plans);
-    const { status } = useSelector((state: RootState) => state.auth);
+    const { status, user } = useSelector((state: RootState) => state.auth);
+    const allSubscriptions = useSelector((state: RootState) => state.subscription.subscriptions);
+    const userSubscriptions = allSubscriptions.filter((sub) => sub.user.id === user?.id)
 
     const dispatch = useDispatch<AppDispatch>();
     const navigate = useNavigate();
@@ -46,6 +48,10 @@ export default function ServicesSlider() {
         setCurrentIndex((prevIndex) => (prevIndex === services.length - 1 ? 0 : prevIndex + 1));
     };
 
+    const isServiceSubscribed = (serviceId: number) => {
+        return userSubscriptions.some((sub) => sub.plan.service.id === serviceId)
+    }
+
     return (
         <>
             <section className=" w-full flex flex-col items-center py-16 pt-0 ">
@@ -55,36 +61,41 @@ export default function ServicesSlider() {
                         className="flex transition-transform duration-700 ease-in-out"
                         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
                     >
-                        {services.map((service, index) => (
-                            <div
-                                key={index}
-                                className={`min-w-full bg-black p-18 pb-24 rounded-2xl shadow-lg text-white flex items-center gap-6 opacity-0 translate-y-6 transition-opacity duration-700 ease-out ${index === currentIndex ? "opacity-100 translate-y-0" : ""
-                                    }`}
-                            >
+                        {services.map((service, index) => {
 
-                                <img src={service.logo} alt="Service" className="w-40 h-40 object-contain" />
+                            const subscribed = isServiceSubscribed(service.id)
+
+                            return ((
+                                <div
+                                    key={index}
+                                    className={`min-w-full bg-black p-18 pb-24 rounded-2xl shadow-lg text-white flex items-center gap-6 opacity-0 translate-y-6 transition-opacity duration-700 ease-out ${index === currentIndex ? "opacity-100 translate-y-0" : ""
+                                        }`}
+                                >
+
+                                    <img src={service.logo} alt="Service" className="w-40 h-40 object-contain" />
 
 
-                                <div className="flex-1 text-left">
-                                    <h3 className="text-2xl font-semibold">{service.name}</h3>
-                                    <p className="mt-2 mb-3 text-justify" style={{ color: "#8a8f98" }}>{service.description}</p>
-                                    <div className="flex justify-between">
-                                        <select name="" id="" className=" cursor-pointer px-2 py-2 rounded-lg" onChange={(e) =>
-                                            setSelectedPlan((prev) => ({
-                                                ...prev,
-                                                [currentService.id]: Number(e.target.value),
-                                            }))
-                                        }>
-                                            {plans.filter(plan => plan.service.id === service.id).map(plan => (
-                                                <option className=" bg-gray-500 cursor-pointer" key={plan.id} value={plan.id}>{plan.name}</option>
-                                            ))}
-                                        </select>
-                                        <button className=" cursor-pointer px-4 py-2 bg-white text-black rounded-lg text-1xl" onClick={() => status === 'authenticated' ? navigate(`/payment/${selectedPlan[currentService.id]}`) : navigate('/login')}>Subscribe</button>
+                                    <div className="flex-1 text-left">
+                                        <h3 className="text-2xl font-semibold">{service.name}</h3>
+                                        <p className="mt-2 mb-3 text-justify" style={{ color: "#8a8f98" }}>{service.description}</p>
+                                        <div className="flex justify-between">
+                                            <select name="" id="" className=" cursor-pointer px-2 py-2 rounded-lg" onChange={(e) =>
+                                                setSelectedPlan((prev) => ({
+                                                    ...prev,
+                                                    [currentService.id]: Number(e.target.value),
+                                                }))
+                                            }>
+                                                {plans.filter(plan => plan.service.id === service.id).map(plan => (
+                                                    <option className=" bg-gray-500 cursor-pointer" key={plan.id} value={plan.id}>{plan.name}</option>
+                                                ))}
+                                            </select>
+                                            <button disabled={subscribed} className=" cursor-pointer px-4 py-2 bg-white text-black rounded-lg text-1xl" onClick={() => status === 'authenticated' ? navigate(`/payment/${selectedPlan[currentService.id]}`) : navigate('/login')}>Subscribe</button>
+                                        </div>
+
                                     </div>
-
                                 </div>
-                            </div>
-                        ))}
+                            ))
+                        })}
                     </div>
 
                     <button onClick={handlePrev} className="btn-slide absolute top-1/2 left-1 -translate-y-1/2  px-4 py-2 rounded-full shadow-md">
