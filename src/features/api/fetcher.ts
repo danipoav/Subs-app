2076a94e0d1ef7a75da13c60887b47d1c05e4196
danipoav <dapoav2002@gmail.com>
2025-04-29 14:49:10 +0200

    Updating repsonsive view

--- src/features/api/fetcher.ts
+++ src/features/api/fetcher.ts
@@ -1,6 +1,6 @@
 export const fetcher = async <T = any>(endpoint: string, options: RequestInit = {}): Promise<T> => {
-    // const url = 'https://k6icxtvsw3.execute-api.eu-west-3.amazonaws.com/api';
-    const url = 'http://localhost:3000/api';
+    const url = 'https://k6icxtvsw3.execute-api.eu-west-3.amazonaws.com/api';
+    // const url = 'http://localhost:3000/api';
     const token = localStorage.getItem('token');
 
     try {
